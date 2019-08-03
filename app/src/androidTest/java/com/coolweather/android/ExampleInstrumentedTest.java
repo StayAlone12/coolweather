98bc56eaac0a483ed3a00e9f65242e54b79c2843
luojun <935384711@qq.com>
2019-08-03 15:47:06 +0800

    修改图标和名称

--- app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/coolweather/android/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.coolweather.android", appContext.getPackageName());
+        assertEquals("com.logo.android", appContext.getPackageName());
     }
 }
